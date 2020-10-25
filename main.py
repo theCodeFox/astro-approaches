0c9e4fa3239b0ba7581c0f3667b440822dc283ed
theCodeFox <akkitta@live.co.uk>
2020-10-25 22:01:18 +0000

    added comments and render template

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # to run: python ./main.py
-from flask import Flask
+from flask import Flask, render_template
 
 # Flask object (helps determine route path)
 app = Flask(__name__)
 
 # @ = decorator - wrap existing function and modify its behaviour
-# route name (normally follows route, common convention to use index for home)
+# route name - normally follows route, common convention to use index for home
+# second argument (methods) defaults to ['GET'], add other methods to list as required
 @app.route('/')
 def index():
     return '<h1>Home</h1>'
