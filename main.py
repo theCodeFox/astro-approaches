bf4b1cc36955ecb17f15695cb1fd093d31221b30
theCodeFox <akkitta@live.co.uk>
2021-02-06 17:52:07 +0000

    added ca logic to create ca and hca lists

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 from flask import Flask, render_template
 from urllib.request import urlopen
 import datetime
+import json
 
 # variables needed for NEO API call
 api_key = open('API_Key.txt','r').read()
@@ -25,7 +26,21 @@ def asteroid_data():
     # next week's close encounters
     with urlopen('https://api.nasa.gov/neo/rest/v1/feed?start_date='+today+'&api_key='+api_key) as r:
         text = r.read()
-        print(text)
+        # object [ date: boolean ]
+        close_approaches_list = json.loads(text)['near_earth_objects']
+        ca_data = {}
+        hca_data = {}
+        # filtered data
+        for dates in close_approaches_list:
+            ca_by_date = []
+            hca_by_date = []
+            for cas in close_approaches_list[dates]:
+                ca_by_date.append(cas['is_potentially_hazardous_asteroid'])
+                if cas['is_potentially_hazardous_asteroid'] == True:
+                    hca_by_date.append(cas['is_potentially_hazardous_asteroid'])
+            ca_data[dates] = len(ca_by_date)
+            hca_data[dates] = len(hca_by_date)
+        print(ca_data,hca_data)
     # return text
 asteroid_data()
 
