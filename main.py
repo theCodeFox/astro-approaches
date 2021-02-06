8eed0a7b95868e033ef2d67465a84f976daadae1
theCodeFox <akkitta@live.co.uk>
2021-02-06 18:13:43 +0000

    added neo logic to main page

--- main.py
+++ main.py
@@ -10,18 +10,7 @@ import json
 api_key = open('API_Key.txt','r').read()
 today = datetime.date.today().strftime('%Y-%m-%d')
 
-# Flask object (helps determine route path)
-app = Flask(__name__)
-
-# @ = decorator - wrap existing function and modify its behaviour
-# route name - normally follows route, common convention to use index for home
-# second argument (methods) defaults to ['GET'], add other methods to list as required
-@app.route('/')
-def index():
-    return render_template('main.html')
-
 # get asteroid info from NEO
-@app.route('/asteroid_data')
 def asteroid_data():
     # next week's close encounters
     with urlopen('https://api.nasa.gov/neo/rest/v1/feed?start_date='+today+'&api_key='+api_key) as r:
@@ -30,6 +19,7 @@ def asteroid_data():
         close_approaches_list = json.loads(text)['near_earth_objects']
         ca_data = {}
         hca_data = {}
+        data = {}
         # filtered data
         for dates in close_approaches_list:
             ca_by_date = []
@@ -40,9 +30,21 @@ def asteroid_data():
                     hca_by_date.append(cas['is_potentially_hazardous_asteroid'])
             ca_data[dates] = len(ca_by_date)
             hca_data[dates] = len(hca_by_date)
-        print(ca_data,hca_data)
-    # return text
-asteroid_data()
+        data['ca'] = ca_data
+        data['hca'] = hca_data
+    return data
+
+# Flask object (helps determine route path)
+app = Flask(__name__)
+
+# @ = decorator - wrap existing function and modify its behaviour
+# route name - normally follows route, common convention to use index for home
+# second argument (methods) defaults to ['GET'], add other methods to list as required
+@app.route('/')
+def index():
+    data = asteroid_data()
+    return render_template('main.html', data=data)
+
 
 # quick variable example
 # @app.route('/profile/<user>')
