938184b9818df14f15afe0b10e24595cec87fefc
theCodeFox <akkitta@live.co.uk>
2021-02-07 00:26:55 +0000

    styled charts

--- main.py
+++ main.py
@@ -16,10 +16,15 @@ def asteroid_data():
     with urlopen('https://api.nasa.gov/neo/rest/v1/feed?start_date='+today+'&api_key='+api_key) as r:
         text = r.read()
         close_approaches_list = json.loads(text)['near_earth_objects']
+        # ca
         ca_data = {}
         total_cas = 0
+        max_ca = 0
+        # hca
         hca_data = {}
         total_hcas = 0
+        max_hca = 0
+        
         data = {}
         # filtered data
         for dates in close_approaches_list:
@@ -30,6 +35,10 @@ def asteroid_data():
                 ca_by_date.append(cas['is_potentially_hazardous_asteroid'])
                 if cas['is_potentially_hazardous_asteroid'] == True:
                     hca_by_date.append(cas['is_potentially_hazardous_asteroid'])
+            if len(ca_by_date) > max_ca:
+                max_ca = len(ca_by_date)
+            if len(hca_by_date) > max_hca:
+                max_hca = len(hca_by_date)
             total_cas = total_cas + len(ca_by_date)
             total_hcas = total_hcas + len(hca_by_date)
             ca_data[dates] = [len(ca_by_date),date]
@@ -39,6 +48,8 @@ def asteroid_data():
         data['hca'] = dict(sorted(hca_data.items()))
         data['total_cas'] = total_cas
         data['total_hcas'] = total_hcas
+        data['max_ca'] = max_ca
+        data['max_hca'] = max_hca
     return data
 
 # Flask object (helps determine route path)
